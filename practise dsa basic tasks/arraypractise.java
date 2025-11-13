1d41ffff7580ae54d9d6853555aef4e189f01210
Mariyamsalman <salmanmariyam47@gmail.com>
2025-11-13 21:58:42 +0500

    array practise with constructor and inputs

--- practise dsa basic tasks/arraypractise.java
+++ practise dsa basic tasks/arraypractise.java
@@ -1,57 +1,50 @@
 import java.util.*;
 public class arraypractise {
+  int size;
+  int capacity;
+  int[] arr;
+    public arraypractise(int capacity) {
+ this.size=0;
+  this.capacity=capacity;
+ arr=new int[capacity];
+    }
     
-int[] arr=new int[5];
-public void insert(){
-arr[0]=1;
-arr[1]=9;
-arr[2]=10;
-arr[3]=3;
 
-//[1,9,10,3,0]
-int index=2;
-
+public void insert(int index,int data){
+if(size==capacity){
+  System.out.println("no more space");
+}else if (size==0) {
+    arr[index]=data;
+    size++;
+}else{
 for(int i=arr.length-1;i>index;i--){
-    arr[i]=arr[i-1];
-      //arr[4]=arr[3]
-      //arr[3] ==arr[2]               
+    arr[i]=arr[i-1];              
 }
-arr[index]=5;
-
+arr[index]=data;
+size++;
   System.out.println(Arrays.toString(arr));
 
 }
+}
 
-public void delete1(){
-    int index=2;
-    int size=4;
-arr[0]=1;
-arr[1]=9;
-arr[2]=10;
-arr[3]=3;
-arr[4]=5 ;
-//[1,9,10,3,5]
-
+public void delete1(int index){
+if(index<0||index>=size){
+  System.out.println("invalid index");
+}else{
 for(int i=index;i<arr.length-1;i++){
     arr[i]=arr[i+1];
-    //arr[2]=arr[3]
-    //arr[3]=arr[4]
   
 }
 size--;
-for (int i = 0; i <=size; i++) {
+for (int i = 0; i <size; i++) {
     System.out.println("after deletion from method 1:"+arr[i]);
 }
 }
-
-public void delete2(){
-int index=2;
-arr[0]=1;
-arr[1]=9;
-arr[2]=10;
-arr[3]=3;
-arr[4]=5 ;
-//[1,9,10,3,5]
+}
+public void delete2(int index){
+if(index<0||index>=size){
+  System.out.println("invalid index");
+}else{
 int[] newarr=new int[arr.length-1];
 for(int i=0;i<index;i++){
     newarr[i]=arr[i];
@@ -65,11 +58,46 @@ for(int i=index;i<arr.length-1;i++){
 }
   System.out.println("after deleting form method 2:"+Arrays.toString(newarr));
 }
+}
 public static void main(String[] args) {
-    arraypractise a=new arraypractise();
-      a.insert();
-      a.delete1();
-      a.delete2();
+   
+    Scanner sc=new Scanner(System.in);
+     System.out.println("give the capacity you want to keep");
+      int capacity=sc.nextInt();
+          arraypractise a=new arraypractise(capacity);
+
+    System.out.println("give the number you want to insert");
+    int num=sc.nextInt();
+        System.out.println("give the index on you want to insert");
+        int index=sc.nextInt();
+            a.insert(index,num);
+      
+            System.out.println("give the number you want to insert");
+        num=sc.nextInt();
+        System.out.println("give the index on you want to insert");
+         index=sc.nextInt();
+             a.insert(index,num);
+           
+             System.out.println("give the number you want to insert");
+        num=sc.nextInt();
+        System.out.println("give the index on you want to insert");
+         index=sc.nextInt();
+             a.insert(index,num);
+           
+             System.out.println("give the number you want to insert");
+        num=sc.nextInt();
+        System.out.println("give the index on you want to insert");
+         index=sc.nextInt();
+             a.insert(index,num);
+           
+          
+         System.out.println("give the index from where you want to delete from method 1");
+       index=sc.nextInt();
+       a.delete1(index);
+       
+       System.out.println("give the index from where you want to delete from method 2");
+       index=sc.nextInt();
+      a.delete2(index);
     }
 
 }
